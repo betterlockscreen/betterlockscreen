51eb7f0f0a10d5b5ed5b7e60d25dbdd9abbb45c2
Sebastian Sellmeier <mail@sebastian-sellmeier.de>
2022-10-09 20:30:26 +0200

    Fix bash_5 -> bash in nix-shell

--- shell.nix
+++ shell.nix
@@ -7,9 +7,9 @@
 with lib;
 
 pkgs.mkShell {
-	buildInputs = with pkgs; [ bash_5 bc coreutils i3lock-color gawk gnugrep gnused imagemagick procps shellcheck xorg.xdpyinfo xorg.xrandr xorg.xset ];
+	buildInputs = with pkgs; [ bash bc coreutils i3lock-color gawk gnugrep gnused imagemagick procps shellcheck xorg.xdpyinfo xorg.xrandr xorg.xset ];
 	shellHooks = ''
-		alias bash5="${pkgs.bash_5}/bin/bash"
+		alias bash5="${pkgs.bash}/bin/bash"
 		alias i3lock="i3lock-color"
 	'';
 }
