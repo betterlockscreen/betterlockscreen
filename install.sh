9c152973289a34a859a3952b4616a05ffbf1c020
Sebastian Sellmeier <mail@sebastian-sellmeier.de>
2023-01-07 17:32:14 +0100

    Update install-script from next (@see hint in #321)

--- install.sh
+++ install.sh
@@ -64,10 +64,15 @@ if ! cmd_exists DEPS["i3lock-color"] && cmd_exists "i3lock"; then
 fi
 
 for key in "${!DEPS[@]}"; do
-	[[ ! -e "$(command -v ${DEPS[$key]})" ]] && echof error "Missing '$key' under binary named '${DEPS[$key]}'!" && exit 1
+	[[ ! -e $(command -v "${DEPS[$key]}") ]] && echof error "Missing '$key' under binary named '${DEPS[$key]}'!" && exit 1
 done
 
 echof ok "done!"
+
+BLI_TEMP_DIR=$(mktemp -d)
+
+git clone https://github.com/betterlockscreen/betterlockscreen "$BLI_TEMP_DIR" &>/dev/null
+cd "$BLI_TEMP_DIR" || exit 1
 
 VERSION=$2
 if [[ $VERSION == "" ]] || [[ $VERSION == "latest" ]]; then
@@ -76,10 +81,7 @@ if [[ $VERSION == "" ]] || [[ $VERSION == "latest" ]]; then
 	echof ok "done! ($VERSION)"
 fi
 
-BLI_TEMP_DIR=$(mktemp -d)
-
-git clone -b "$VERSION" https://github.com/betterlockscreen/betterlockscreen "$BLI_TEMP_DIR" &>/dev/null
-cd "$BLI_TEMP_DIR" || exit 1
+git checkout "$VERSION" &>/dev/null
 
 echof info "Installing Betterlockscreen to '$BL_INSTALL_DIR'... "
 cp betterlockscreen "$BL_INSTALL_DIR"
